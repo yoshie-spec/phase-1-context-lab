2569c60f9d88786090c892de101d995bb9bc1cf5
yosselin alvarez <yoshie_9311@icloud.com>
2023-04-30 22:05:48 -0400

    complete lap

--- index.js
+++ index.js
@@ -21,3 +21,70 @@ const allWagesFor = function () {
     return payable
 }
 
+
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+    return {
+        firstName: firstName,
+        familyName: familyName,
+        title: title,
+        payPerHour: payPerHour,
+        timeInEvents:[],
+        timeOutEvents:[],
+    }
+}
+
+function createEmployeeRecords(array) {
+    let newArrayOfRecords = array.map(record => createEmployeeRecord(record));
+    return newArrayOfRecords;
+}
+
+function createTimeInEvent(dateStamp) {
+    let stringHour = dateStamp.substring(11);
+    let hour = parseInt(stringHour, 10);
+    let date = dateStamp.substring(0, 10);
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: hour,
+        date: `${date}`
+    })
+    return this;
+}
+
+function createTimeOutEvent(dateStamp) {
+    let stringHour = dateStamp.substring(11);
+    let hour = parseInt(stringHour, 10);
+    let date = dateStamp.substring(0, 10);
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: hour,
+        date: `${date}`
+    })
+    return this;
+}
+
+function hoursWorkedOnDate(dateOfForm) {
+    let shiftStartObj = this.timeInEvents.find(obj => obj.date === dateOfForm);
+    let shiftEndObj = this.timeOutEvents.find(obj => obj.date === dateOfForm);
+    let timeIn = shiftStartObj.hour;
+    let timeOut = shiftEndObj.hour;
+    return (timeOut/100) - (timeIn/100);
+}
+
+function wagesEarnedOnDate(dateOfForm) {
+    let hours = hoursWorkedOnDate.call(this, dateOfForm);
+    return hours * (this.payPerHour);
+}
+
+function calculatePayroll(array) {
+    let allWagesOwed = array.map(record => allWagesFor.call(record));
+    let sumOfAllWagesOwed = 0;
+    allWagesOwed.forEach(wage => {
+        sumOfAllWagesOwed += wage;
+    });
+    return sumOfAllWagesOwed;
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    let found = srcArray.find(record => record.firstName === firstName);
+    return found;
+}
